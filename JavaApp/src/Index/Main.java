7048de6c336ae094f13dc601b4a15ade166ce085
SmartAnubhab <smartanubhab999@gmail.com>
2021-07-28 16:43:35 +0530

    Made a new array class to compare tow arrays, we can use this code to compare tow phone numbers as the length of the integer arrays should be upto 10

--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -1,14 +1,10 @@
 package Index;
-import arrays.compareArray;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] firstArray={8,2,5,0,8,9,1,4,9,9};
-        int[] secondArray={9,9,0,3,0,1,4,7,8,8};
-        int[] thirdArray={8,2,5,0,8,9,1,4,9,9};
 
-        compareArray com=new compareArray(firstArray, secondArray);
+
 
     }
 }
