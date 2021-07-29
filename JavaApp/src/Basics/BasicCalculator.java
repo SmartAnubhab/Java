2d4873986636dae18014d7eb1337b2c3449b49e4
SmartAnubhab <smartanubhab999@gmail.com>
2021-07-29 23:22:42 +0530

    made all the four add, sub, mul and div methods

--- JavaApp/src/Basics/BasicCalculator.java
+++ JavaApp/src/Basics/BasicCalculator.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 
 public class BasicCalculator {
     Scanner scanner=new Scanner(System.in);
-    int a;
-    int b;
+    private int a;
+    private int b;
     int result;
 
-    public void add(int a, int b){
+    public BasicCalculator(int a, int b){
         this.a=a;
         this.b=b;
+    }
+
+    public void add(){
         result=a+b;
-        System.out.println("The sum is : "+result);
+        System.out.println("The sum is : " + result);
+    }
+    public void sub(){
+        result=a-b;
+        System.out.println("The sub is : " + result);
+    }
+    public void mul(){
+        result=a*b;
+        System.out.println("The multiplication is : " + result);
+    }
+    public void div(){
+        result=a/b;
+        System.out.println("The divition is : " + result);
     }
 }
--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -1,13 +1,9 @@
 package Index;
-import Basics.BasicCalculator;
-import Basics.ReverseAString;
 
 public class Main {
 
     public static void main(String[] args) {
-        BasicCalculator bc=new BasicCalculator();
 
-        bc.add(12,45);
 
 
     }
