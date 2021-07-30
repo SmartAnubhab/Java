468de352711575801b568ac1c0a3d43406ce809e
SmartAnubhab <smartanubhab999@gmail.com>
2021-07-30 20:21:12 +0530

    demo while loop test done

--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -1,10 +1,21 @@
 package Index;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String name="ANUBHAB";
+        Scanner scanner=new Scanner(name);
+        ArrayList<String> nameArray=new ArrayList<>();
 
+        while(scanner.hasNext()){
+            nameArray.add(scanner.next());
+        }
+
+        System.out.println(nameArray);
 
     }
 }
