c6ea7cdef07c750c32270787c28c868b61ab0e4d
SmartAnubhab <smartanubhab999@gmail.com>
2021-07-31 16:34:28 +0530

    String input calculator shifted to different class in the package called Basics

--- JavaApp/src/Basics/StringInputCalculator.java
+++ JavaApp/src/Basics/StringInputCalculator.java
@@ -1,4 +1,20 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class StringInputCalculator {
+    int a,b;
+    int result;
+    Scanner scanner = new Scanner(System.in);
+
+    public void add() {
+        System.out.println("Enter the first value: ");
+        a = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the second value: ");
+        b = Integer.parseInt(scanner.nextLine());
+        result = a + b;
+
+        System.out.println("The result is : " + result);
+
+    }
 }
--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -1,23 +1,10 @@
 package Index;
 import Basics.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int a,b;
-        int result;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first value: ");
-        a=Integer.parseInt(scanner.nextLine());
-        System.out.println("Enter the second value: ");
-        b=Integer.parseInt(scanner.nextLine());
-        result=a+b;
-
-        System.out.println("The result is : " + result);
     }
 }
