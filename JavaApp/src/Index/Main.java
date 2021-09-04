f87942de8891ab2507b7cd60a5c879caa495c7f0
SmartAnubhab <smartanubhab999@gmail.com>
2021-09-05 01:06:14 +0530

    some file testing is done for Scanner, Running ok.

--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -1,10 +1,17 @@
 package Index;
-import Basics.*;
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args){
+        System.out.println("CALCULATOR");
+        Scanner scanner=new Scanner(System.in);
 
-public class Main {
-
-    public static void main(String[] args) {
+        System.out.println("ENTER FIRST VALUE ");
+        float value1=scanner.nextFloat();
+        System.out.println("ENTER SECOND VALUE ");
+        float value2=scanner.nextFloat();
+        float result=value1+value2;
+            System.out.println("THE RESULT IS " + result);
 
     }
 }
