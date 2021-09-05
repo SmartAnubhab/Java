fdff794f852d94ff9f12e45ec3bc6bbc1e64fe7e
SmartAnubhab <smartanubhab999@gmail.com>
2021-09-06 00:42:41 +0530

    object datatype variable tested

--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        int[] arr=new int[5];
-        Scanner scanner=new Scanner(System.in);
+        Object[] objects = new Object[5];
+        objects[0] = "Anubhab Mondal";
+        objects[1] = 123;
+        objects[2] = 'A';
+        objects[3] = 12.15;
+        objects[4] = true;
 
-        for(int i=1; i<=5; i++) {
-            System.out.println("ENTER THE NUMBER " + i + " : ");
-            arr[i-1] = scanner.nextInt();
+        for (Object each: objects){
+            System.out.println(each);
         }
-        System.out.println("The stored values are : ");
-        for (int a: arr){
-            System.out.print(a + " ");
-        }
+
     }
 }
