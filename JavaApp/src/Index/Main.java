db608e3b218fdfb7634eae34327fd9f9752d0fde
SmartAnubhab <smartanubhab999@gmail.com>
2021-09-06 16:10:06 +0530

    static method

--- JavaApp/src/Index/Main.java
+++ JavaApp/src/Index/Main.java
@@ -1,28 +1,16 @@
 package Index;
 import java.util.Scanner;
 
+
+class PrintName{
+    public static void myName(String name){
+        System.out.println("My name is " + name);
+    }
+}
 public class Main{
     public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Please Enter a number between 1 to 3");
-        int a = scanner.nextInt();
-        String month = null;
-        switch(a){
-            case 1:
-                month = "January";
-                break;
-            case 2:
-                month = "February";
-                break;
-            case 3:
-                month = "March";
-                break;
-            default:
-                System.out.println("month is unknown");
-        }
-        if (month != null) {
-            System.out.println(month);
-        }
+        PrintName.myName("Anubhab Mondal");
+        PrintName.myName("Ankita Mondal");
 
     }
 }
