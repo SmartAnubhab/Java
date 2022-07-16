a5492711aea1308a9685643d93d633279a0229ea
SmartAnubhab <smartanubhab999@gmail.com>
2022-07-16 23:06:33 +0530

    tested

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        try{
-//            int y=10/0;
-            boolean b=false;
-
-
-        } catch (ArithmeticException e){
-            System.out.println("arithmetic exception");
-        } catch (Exception e){
-            System.out.println("normal exception");
-        }
+        System.out.println("hello my name is Anubhab Mondal");
 
     }
 }
