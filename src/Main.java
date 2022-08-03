4a9b8a93134057ec7e71193ce212f7bb89ac0f5b
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-03 20:35:08 +0530

    making a calculator

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello my name is Anubhab Mondal");
+        System.out.println("calculator:");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter number 1 : ");
+        int num1=scanner.nextInt();
+        System.out.println("Enter number 2 : ");
+        int num2=scanner.nextInt();
 
+        int result=num1+num2;
+
+        System.out.println("Result is ");
     }
 }
