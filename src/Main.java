f238dd89852d9e850959903d15e8e983aca805cc
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-05 23:44:01 +0530

    removed the demo code for calculator

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,6 @@ public class Main {
         System.out.println("CALCULATOR: ");
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter 1st value : ");
-        int num1=scanner.nextInt();
-        System.out.println("Enter 2nd value : ");
-        int num2=scanner.nextInt();
-
-        int result=num1+num2;
-
-        System.out.println("Result is : " + result);
+        
     }
 }
