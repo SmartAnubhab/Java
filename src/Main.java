324cce1fc23a0ac64cd9983a1f65a3cd61f52491
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-05 23:45:12 +0530

    deleted the header file for CALCULATOR

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("CALCULATOR: ");
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter 1st value : ");
-        
+
+
     }
 }
